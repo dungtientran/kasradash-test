c542ce2818ea34757bb775a652d69ab5ec379fc8
dungtientran <119165039+dungtientran@users.noreply.github.com>
2025-05-21 06:59:35 +0700

    chore(ui): clean up unused classes in header

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Container } from "@/components/layout/container";
 import { CardSettingForm } from "@/features/setting/components/card-setting-form";
 import { ThemeToggle } from "@/components/layout/theme-toggle";
+import { Separator } from "@/components/ui/separator";
 
 const SettingPage = () => {
   return (
     <Container>
-      <div className="space-y-10 w-full">
+      <div className=" w-full">
         <div className="flex items-center gap-x-2.5">
           Setting theme
           <ThemeToggle />
         </div>
-        <div className=" flex  justify-center">
+        <Separator className="my-5" />
+        <div className="flex justify-center">
           <CardSettingForm />
         </div>
       </div>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,7 +6,7 @@ import { Separator } from "../ui/separator";
 export const Header = () => {
   return (
     <>
-      <header className="flex h-16 shrink-0 px-4 md:px-6 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+      <header className="flex h-16 shrink-0 px-4 md:px-6 items-center justify-between gap-2 ">
         <div className="flex items-center gap-2">
           <SidebarTrigger />
           <h2 className="text-xl lg:text-2xl">Admin Dashboard</h2>
