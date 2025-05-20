7b57fc38d41e569443738fe0c842100c9d8ff136
dungtientran <119165039+dungtientran@users.noreply.github.com>
2025-05-21 03:38:50 +0700

    feat: create sidebar

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "@/components/layout/container";
+
+const SettingPage = () => {
+  return <Container>SettingPage</Container>;
+};
+
+export default SettingPage;
